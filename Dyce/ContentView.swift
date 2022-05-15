ebbe1c0982452755a30967344c8db9a78f0da900
Adriana Belinski <adrianab@protonmail.com>
2022-05-15 16:22:28 -0500

    Added button.

--- Dyce/ContentView.swift
+++ Dyce/ContentView.swift
@@ -2,8 +2,14 @@ import SwiftUI
 
 struct ContentView: View {
   var body: some View {
-    Text("Hello, world!")
+    Button("Roll Dice", action: {})
+      .font(.title2)
       .padding()
+      .foregroundColor(.blue)
+      .background(
+        RoundedRectangle(cornerRadius: 20)
+          .stroke(Color.gray, lineWidth: 2)
+      )
   }
 }
 
