77d107de0b2a1dc2cca96055b828577587389db2
Adriana Belinski <adrianab@protonmail.com>
2022-05-15 16:08:08 -0500

    Removed comments and fixed indentation

--- Dyce/ContentView.swift
+++ Dyce/ContentView.swift
@@ -1,21 +1,14 @@
-//
-//  ContentView.swift
-//  Dyce
-//
-//  Created by Adriana Belinski on 5/15/22.
-//
-
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        Text("Hello, world!")
-            .padding()
-    }
+  var body: some View {
+    Text("Hello, world!")
+      .padding()
+  }
 }
 
 struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
+  static var previews: some View {
+    ContentView()
+  }
 }
--- Dyce/DyceApp.swift
+++ Dyce/DyceApp.swift
@@ -1,17 +1,10 @@
-//
-//  DyceApp.swift
-//  Dyce
-//
-//  Created by Adriana Belinski on 5/15/22.
-//
-
 import SwiftUI
 
 @main
 struct DyceApp: App {
-    var body: some Scene {
-        WindowGroup {
-            ContentView()
-        }
+  var body: some Scene {
+    WindowGroup {
+      ContentView()
     }
+  }
 }
