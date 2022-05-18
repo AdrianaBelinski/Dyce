bedbc5c5726f6f09efbf69cd534704f05da0f04c
Adriana Belinski <adrianab@protonmail.com>
2022-05-18 17:47:58 -0500

    Displayed dice in a lazy V grid.

--- Dyce/DyceResultsView.swift
+++ Dyce/DyceResultsView.swift
@@ -2,12 +2,20 @@ import SwiftUI
 
 struct DyceResultsView: View {
   let dice: [Int]
+  
+  let threeColumnGrid = [
+    GridItem(.flexible()),
+    GridItem(.flexible()),
+    GridItem(.flexible())
+  ]
 
   var body: some View {
-    ForEach(0 ..< dice.count, id: \.self) { i in
-      Image(systemName: "die.face.\(dice[i])")
-        .resizable()
-        .frame(width: 100, height: 100)
+    LazyVGrid(columns: threeColumnGrid) {
+      ForEach(0 ..< dice.count, id: \.self) { i in
+        Image(systemName: "die.face.\(dice[i])")
+          .resizable()
+          .frame(width: 100, height: 100)
+      }
     }
   }
 }
